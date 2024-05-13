6978f820e1111a6f2525ec9561be861f88f29ee7
Andrey <ae55@yandex.ru>
2024-05-13 03:20:32 +0300

    stage2 try2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,117 @@
 import '../pages/index.css';
 
 import { initialCards } from './cards.js';
-import { addCardsSet } from './card.js';
-import { enablePopup } from './modal.js';
+import { handleDeleteCard, handleClickLike, createCard } from './card.js';
+import { showPopup, hidePopup, enablePopupAnimation } from './modal.js';
 
-export function handleDeleteCard(evt) {
-  evt.target.closest('.card').remove();
+const popupProfile = document.querySelector('.popup_type_edit');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const popupImage = document.querySelector('.popup_type_image');
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const popupImageImg = document.querySelector('.popup__image');
+const popupImageCaption = document.querySelector('.popup__caption');
+const cardsList = document.querySelector('.places__list');
+const buttonNewCard = document.querySelector('.profile__add-button');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const popupProfileForm = popupProfile.querySelector('.popup__form');
+const popupNewCardForm = popupNewCard.querySelector('.popup__form');
+const inputCardName = popupNewCard.querySelector('input[name="place-name"]');
+const inputCardLink = popupNewCard.querySelector('input[name="link"]');
+const inputProfileName = popupProfile.querySelector('input[name="name"]');
+const inputProfileDescription = popupProfile.querySelector('input[name="description"]');
+
+
+function loadInfoFromProfile() {
+  inputProfileName.value = profileName.textContent;
+  inputProfileDescription.value = profileDescription.textContent;
 }
 
-export function handleClickImage(evt) {
-  const popupImage = document.querySelector('.popup__image');
-  popupImage.src = evt.target.src;
-  popupImage.alt = evt.target.alt;
-  document.querySelector('.popup__caption').textContent = evt.target.alt;
+function saveInfoToProfile() {
+  profileName.textContent = inputProfileName.value;
+  profileDescription.textContent = inputProfileDescription.value;
 }
 
-export function handleClickLike(evt) {
-  evt.target.classList.toggle('card__like-button_is-active');
+function clearCardInfo() {
+  inputCardName.value = '';
+  inputCardLink.value = '';
+}
+
+function setPopupImageInfo(link, name) {
+  popupImageImg.src = link;
+  popupImageImg.alt = name;
+  popupImageCaption.textContent = name;
+}
+
+function handleClickImage(link, name) {
+  setPopupImageInfo(link, name);
+  showPopup(popupImage, true);
+}
+
+function addCard(cardData, prepend = false) {
+  const newCard = createCard(cardData, handleDeleteCard, handleClickImage, handleClickLike);
+  if (prepend) {
+    cardsList.prepend(newCard);
+  } else {
+    cardsList.append(newCard);
+  }
+}
+
+function addCardsSet(cardsSet) {
+  cardsSet.forEach(cardData => {
+    addCard(cardData);
+  });
+}
+
+function addCardFromPopup() {
+  const cardData = {};
+  cardData.name = inputCardName.value;
+  cardData.link = inputCardLink.value;
+
+  if (cardData.name != '' && cardData.link != '') {
+    addCard(cardData, true);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleOpenPopupNewCard() {
+  clearCardInfo();
+  showPopup(popupNewCard);
+}
+
+function handleSubmitPopupNewCard(evt) {
+  evt.preventDefault();
+  if (addCardFromPopup()) hidePopup(popupNewCard);
+}
+
+function enablePopupNewCard() {
+  buttonNewCard.addEventListener('click', handleOpenPopupNewCard);
+  popupNewCardForm.addEventListener('submit', handleSubmitPopupNewCard);
+}
+
+function handleOpenPopupProfile() {
+  loadInfoFromProfile();
+  showPopup(popupProfile);
+}
+
+function handleSubmitPopupProfile(evt) {
+  evt.preventDefault();
+  saveInfoToProfile();
+  hidePopup(popupProfile);
+}
+
+function enablePopupProfile() {
+  buttonEditProfile.addEventListener('click', handleOpenPopupProfile);
+  popupProfileForm.addEventListener('submit', handleSubmitPopupProfile);
 }
 
 addCardsSet(initialCards);
 
-enablePopup(document.querySelector('.profile__edit-button'), document.querySelector('.popup_type_edit'), 'profile');
-enablePopup(document.querySelector('.profile__add-button'), document.querySelector('.popup_type_new-card'), 'card');
-enablePopup(document.querySelector('.places__list'), document.querySelector('.popup_type_image'), 'image');
+enablePopupAnimation(popupProfile);
+enablePopupAnimation(popupNewCard);
+enablePopupAnimation(popupImage);
+
+enablePopupNewCard();
+enablePopupProfile();
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,74 +1,33 @@
-import { addCardFromPopup } from './card.js';
-
-function loadInfoFromProfile() {
-  const popup = document.querySelector('.popup_type_edit');
-  const inputName = popup.querySelector('input[name="name"]');
-  const inputDescription = popup.querySelector('input[name="description"]');
-  const profileName = document.querySelector('.profile__title');
-  const profileDescription = document.querySelector('.profile__description');
-
-  inputName.value = profileName.textContent;
-  inputDescription.value = profileDescription.textContent;
+export function showPopup(popup, click = false) {
+  if (click) {
+    document.addEventListener('click', handleClosePopup);
+  } else {
+    document.addEventListener('mousedown', handleClosePopup);
+  }
+  document.addEventListener('keydown', handleClosePopup);
+  popup.classList.add('popup_is-opened');
 }
 
-function saveInfoToProfile() {
-  const popup = document.querySelector('.popup_type_edit');
-  const inputName = popup.querySelector('input[name="name"]');
-  const inputDescription = popup.querySelector('input[name="description"]');
-  const profileName = document.querySelector('.profile__title');
-  const profileDescription = document.querySelector('.profile__description');
-
-  profileName.textContent = inputName.value;
-  profileDescription.textContent = inputDescription.value;
+export function hidePopup(popup) {
+  document.removeEventListener('click', handleClosePopup);
+  document.removeEventListener('mousedown', handleClosePopup);
+  document.removeEventListener('keydown', handleClosePopup);
+  popup.classList.remove('popup_is-opened');
 }
 
-function clearCardInfo() {
-  const popup = document.querySelector('.popup_type_new-card');
-  const inputName = popup.querySelector('input[name="place-name"]');
-  const inputLink = popup.querySelector('input[name="link"]');
-
-  inputName.value = '';
-  inputLink.value = '';
+export function enablePopupAnimation(popup) {
+  popup.classList.add('popup_is-animated');
 }
 
 function handleClosePopup(evt) {
   const popup = document.querySelector('.popup_is-opened');
-  let eventTargetClassList;
-  if (evt) eventTargetClassList = evt.target.classList;
+  const eventTargetClassList = evt.target.classList;
 
   if (
-      !evt
-      || eventTargetClassList.contains('popup_is-opened')
+      eventTargetClassList.contains('popup_is-opened')
       || eventTargetClassList.contains('popup__close')
       || eventTargetClassList.contains('popup__image')
       || evt.key === 'Escape'
      )
-       {
-        popup.classList.remove('popup_is-opened');
-        document.removeEventListener('click', handleClosePopup);
-        document.removeEventListener('keydown', handleClosePopup);
-       }
-}
-
-export function enablePopup(button, popup, popupType) {
-  popup.classList.add('popup_is-animated');
-
-  button.addEventListener('click', function(evt) {
-    if (popupType !== 'image' || evt.target.classList.contains('card__image')) {
-      popup.classList.add('popup_is-opened');
-      document.addEventListener('click', handleClosePopup);
-      document.addEventListener('keydown', handleClosePopup);
-      if (popupType === 'profile') loadInfoFromProfile();
-      if (popupType === 'card') clearCardInfo();
-    }
-  });
-
-  if (popupType === 'profile' || popupType === 'card') {
-    popup.addEventListener('submit', function(evt) {
-      evt.preventDefault();
-      if (popupType === 'profile') saveInfoToProfile();
-      if (popupType === 'card') addCardFromPopup();
-      handleClosePopup();
-    });
-  }
+       hidePopup(popup);
 }
